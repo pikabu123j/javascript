bda5ee8064cda312b2368152c46b64315088ab92
Ritika jain <101181189+pikabu123j@users.noreply.github.com>
2025-02-06 14:51:44 +0000

    commit it

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version 
+
+//alert(3+3) //we are using nodejs not browser
+
+let name = "rit"
+let age = 20
+let isgood = false
+let state;
+
+//  DATATYPES
+/*
+number - range 2 to the power 53
+bigint 
+string - ""
+boolean = true/false
+null = standalone value 
+undefined 
+symbol = unique
+*/
+
+console.log(typeof "riya");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accID = 12345
+let accEmail = "ritika@google.com"
+var accPass = "123456"
+accCity = "jaipur"
+
+// accID = 5678
+// console.log(accID);
+accEmail = "hello@google.com"
+accPassv= "9898"
+accCity = "Bengaluru"
+
+console.table([accID,accEmail,accPass,accCity])
+
+// IMPORTANT : prefer not use var bcz of issue in block scope and functional scope
