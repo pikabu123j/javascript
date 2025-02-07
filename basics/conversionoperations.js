433016dc4408e5fbc79c46f60f094ae665894fc9
Ritika jain <101181189+pikabu123j@users.noreply.github.com>
2025-02-07 15:20:44 +0000

    commit this changes

---
+++ basics/conversionoperations.js
@@ -0,0 +1,26 @@
+let score = "33abc" //NaN
+// let score = null //0
+// let score = undefined //NaN
+// let score = true // 1
+// let score = "ritika" //NaN
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = 1//true
+// let isLoggedIn = "" // false
+// let isLoggedIn = "ritika" //true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
